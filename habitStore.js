1f05b457ec43550c5d1a829557b387776ca70caf
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2023-02-19 21:18:33 +0500

    various changes

---
+++ habitStore.js
@@ -0,0 +1,69 @@
+const { DateTime } = require("luxon");
+
+module.exports = {
+  programming: [
+    {
+      // level 1
+      dateFrom: DateTime.fromISO("2023-02-19"),
+      dateTo: DateTime.fromISO("2023-03-19"),
+      data: ["LeetCode", "Medium"],
+    },
+    {
+      // level 2
+      dateFrom: DateTime.fromISO("2023-03-20"),
+      dateTo: DateTime.fromISO("2023-04-20"),
+      data: ["LeetCode Medium", "Medium 2 articles"],
+    },
+    {
+      // level 3
+      dateFrom: DateTime.fromISO("2023-04-21"),
+      dateTo: DateTime.fromISO("2023-05-21"),
+      data: ["LeetCode Hard", "Medium: write article"],
+    },
+  ],
+  rich: [
+    {
+      dateFrom: DateTime.fromISO("2023-02-19"),
+      dateTo: DateTime.fromISO("2023-03-19"),
+      data: ["Rich"],
+    },
+    {
+      dateFrom: DateTime.fromISO("2023-03-20"),
+      dateTo: DateTime.fromISO("2023-04-20"),
+    },
+    {
+      dateFrom: DateTime.fromISO("2023-04-21"),
+      dateTo: DateTime.fromISO("2023-05-21"),
+    },
+  ],
+  health: [
+    {
+      dateFrom: DateTime.fromISO("2023-02-19"),
+      dateTo: DateTime.fromISO("2023-03-19"),
+      data: ["HEALTH"],
+    },
+    {
+      dateFrom: DateTime.fromISO("2023-03-20"),
+      dateTo: DateTime.fromISO("2023-04-20"),
+    },
+    {
+      dateFrom: DateTime.fromISO("2023-04-21"),
+      dateTo: DateTime.fromISO("2023-05-21"),
+    },
+  ],
+  effective: [
+    {
+      dateFrom: DateTime.fromISO("2023-02-19"),
+      dateTo: DateTime.fromISO("2023-03-19"),
+      data: ["EFFECTIVE"],
+    },
+    {
+      dateFrom: DateTime.fromISO("2023-03-20"),
+      dateTo: DateTime.fromISO("2023-04-20"),
+    },
+    {
+      dateFrom: DateTime.fromISO("2023-04-21"),
+      dateTo: DateTime.fromISO("2023-05-21"),
+    },
+  ],
+};
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const habits = getAllHabits();
+const { DateTime } = require("luxon");
+const habitStore = require("./habitStore");
 
 function getAllHabits() {
-  return {
-    programming: getCurrentProgrammingHabits(),
-    rich: getCurrentRichHabits(),
-    health: getCurrentHealthHabits(),
-    effective: getCurrentEffectiveHabits(),
-  };
+  const allCurrentHabits = {};
+
+  Object.keys(habitStore).forEach((habitArea) => {
+    const [currentHabit] = habitStore[habitArea].filter(
+      (habit) =>
+        habit.dateFrom < DateTime.now() && habit.dateTo > DateTime.now()
+    );
+
+    allCurrentHabits[habitArea] = currentHabit.data;
+  });
+
+  return allCurrentHabits;
 }
 
-function getCurrentProgrammingHabits() {}
-
-function getCurrentRichHabits() {}
-
-function getCurrentHealthHabits() {}
-
-function getCurrentEffectiveHabits() {}
+console.log(getAllHabits());
