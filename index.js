41c77318bf5a46db63fc125b59f9ae9936da4530
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2023-02-19 20:47:48 +0500

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const habits = getAllHabits();
+
+function getAllHabits() {
+  return {
+    programming: getCurrentProgrammingHabits(),
+    rich: getCurrentRichHabits(),
+    health: getCurrentHealthHabits(),
+    effective: getCurrentEffectiveHabits(),
+  };
+}
+
+function getCurrentProgrammingHabits() {}
+
+function getCurrentRichHabits() {}
+
+function getCurrentHealthHabits() {}
+
+function getCurrentEffectiveHabits() {}
